ed42e1664a89c54f96235cf38115048709dda310
Andrey Cherevichnik <cherevichnik.av@edu.spbstu.ru>
2021-12-26 03:59:08 +0200

    Partially finished part 3 and 4

--- src/Part3/Tasks.hs
+++ src/Part3/Tasks.hs
@@ -1,25 +1,43 @@
 module Part3.Tasks where
 
 import Util (notImplementedYet)
+import Data.List
+import Data.Char(digitToInt)
 
 -- Функция finc принимает на вход функцию f и число n и возвращает список чисел [f(n), f(n + 1), ...]
 finc :: (Int -> a) -> Int -> [a]
-finc = notImplementedYet
+finc f n = (f n) : (finc f $ n + 1)
 
 -- Функция ff принимает на вход функцию f и элемент x и возвращает список [x, f(x), f(f(x)), f(f(f(x))) ...]
-ff :: (a -> a) -> a -> [a]
-ff = notImplementedYet
+-- ff :: (a -> a) -> a -> [a]
+ff f x = let
+  nxt y = (f y) : (nxt (f y))
+  in
+    x : (nxt x)
 
 -- Дан список чисел. Вернуть самую часто встречающуюся *цифру* в этих числах (если таковых несколько -- вернуть любую)
 mostFreq :: [Int] -> Int
-mostFreq = notImplementedYet
+mostFreq a = snd $ foldl (\s x -> if length x > fst s then (length x, digitToInt $ head x) else s) (0,0) groupedArr
+  where groupedArr = group (sort (concatMap show a))
 
 -- Дан список lst. Вернуть список элементов из lst без повторений, порядок может быть произвольным.
 uniq :: (Eq a) => [a] -> [a]
 uniq = notImplementedYet
+--uniq a = let
+--  eqGroup | acc [] = acc
+--          | acc (x : xs) = if find (\y -> y == x) acc /= Nothing then eqGroup acc xs else eqGroup (x : acc) xs
+--  in
+--  map (\x -> head x) $ eqGroup [] a
 
 -- Функция grokBy принимает на вход список Lst и функцию F и каждому возможному
 -- значению результата применения F к элементам Lst ставит в соответствие список элементов Lst,
 -- приводящих к этому результату. Результат следует представить в виде списка пар.
 grokBy :: (Eq k) => (a -> k) -> [a] -> [(k, [a])]
-grokBy f l = notImplementedYet
+grokBy = notImplementedYet
+--grokBy f l = let
+--  helper acc [] = []
+--  helper acc (x:xs) = if findGroup x acc /= Nothing then (x : snd (findGroup x acc))  else helper ((f x, [x]) : acc) xs
+--  in
+--    helper [] l
+--  where 
+--    findGroup x arr = find (\y -> fst y == f x) arr
--- src/Part4/Tasks.hs
+++ src/Part4/Tasks.hs
@@ -2,6 +2,7 @@ module Part4.Tasks where
 
 import Util(notImplementedYet)
 
+
 -- Перевёрнутый связный список -- хранит ссылку не на последующию, а на предыдущую ячейку
 data ReverseList a = REmpty | (ReverseList a) :< a
 infixl 5 :<
@@ -16,17 +17,40 @@ rlistToList lst =
 
 -- Реализуйте обратное преобразование
 listToRlist :: [a] -> ReverseList a
-listToRlist = notImplementedYet
+listToRlist lst =
+    reversed (reverse lst)
+    where reversed [] = REmpty
+          reversed (first : tl) = (reversed tl) :< first
 
 -- Реализуйте все представленные ниже классы (см. тесты)
-instance Show (ReverseList a) where
+instance Show a => Show (ReverseList a) where
+    show REmpty = "[]"
+    show lst = let
+      helper REmpty = "" 
+      helper (REmpty :< lst) = show lst
+      helper (hd :< lst) = helper hd ++ "," ++ show lst
+      in 
+      "[" ++ helper lst ++ "]" 
     showsPrec = notImplementedYet
-    show = notImplementedYet
-instance Eq (ReverseList a) where
-    (==) = notImplementedYet
-    (/=) = notImplementedYet
+instance Eq a => Eq (ReverseList a) where
+    (==) REmpty REmpty = True
+    (==) _ REmpty = False
+    (==) REmpty _ = False
+    (==) (lprt :< lLast) (rprt :< rLast) = if lLast /= rLast then False else (==) lprt rprt
+    (/=) trw wer = not $ (trw == wer)
 instance Semigroup (ReverseList a) where
+    (<>) REmpty REmpty = REmpty
+    (<>) REmpty rl = rl
+    (<>) rl REmpty = rl
+    (<>) lRL (rHead :< rLast) = helper ((<>) lRL rHead) rLast
+      where helper rl eRL = (rl :< eRL)
 instance Monoid (ReverseList a) where
+    mempty = REmpty
 instance Functor ReverseList where
+    fmap _ REmpty = REmpty
+    fmap f (rHead :< rLast) = (fmap f rHead) :< f rLast
 instance Applicative ReverseList where
+    pure a = REmpty :< a
+    (<*>) = notImplementedYet
 instance Monad ReverseList where
+    (>>=) = notImplementedYet
